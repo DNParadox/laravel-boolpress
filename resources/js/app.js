e53f7556b748dc3ebe9d869d6bdf722b77006072
DNParadox <m96francesco@gmail.com>
2022-09-02 17:59:21 +0200

    Push

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,9 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
+ import App from './views/App.vue';
+
+ const app = new Vue({
+     el: '#root',
+     render: h => h(App)
+ });
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')
+        ->name('home');
+    });
+
+    Route::get('{any?}', function () {
+        return view('guest.home');
+    });
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
