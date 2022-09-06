e798454ab82ec8e8f954478cb6c54f56e2d5d6c2
DNParadox <m96francesco@gmail.com>
2022-09-06 16:57:01 +0200

    Carbon added

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <a href="{{ route('admin.posts.edit', ['post' => $post->id]) }}" class="btn btn-primary">Modifica Post</a>
     
-    
+
     <form class="mt-2" action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="post">
         @csrf
         @method('delete')
@@ -13,8 +13,8 @@
 
     <h1>{{ $post->title }}</h1>
 
-    <div><strong>Creato il:  </strong> {{ $post->created_at }}</div>
-    <div><strong>Aggiornato il:  </strong> {{ $post->updated_at }}</div>
+    <div><strong>Creato il:  </strong> {{ $post->created_at->format(' d F Y') }}</div>
+    <div><strong>Aggiornato il:  </strong> {{ $post->updated_at->format('d F Y') }}</div>
     <div><strong>Slug:</strong> {{ $post->slug }}</div>
 
     <h3 class="mt-3">Contenuto: </h3>
